0a08e3eb6a491d0b7098843b8d026ffeb9e83469
TeoBelo <64974940+TeoBelo@users.noreply.github.com>
2020-05-07 18:44:44 +0200

    Add files via upload

---
+++ dati_orbite.m
@@ -0,0 +1,22 @@
+%% orbita 1
+% vector_r0: vettore posizione iniziale [km]
+% vector_v0: vettore velocità iniziale [km/s]
+
+vector_r0=[-5183.4184 6189.4459 4384.1737];
+vector_v0=[-5.3130 -4.3350 0.2136];
+
+%% orbita 2
+% vector_e: vettore eccentricità
+% e: eccentricità
+% a: semiasse maggiore [km]
+% omega: ascensione retta del nodo ascendente [rad]
+% i: inclinazione [rad]
+% w: anomalia pericentro [rad]
+% teta: anomalia reale [rad]
+
+a=12610,0;
+e=0,2656;
+i=1,0430;
+omega=1,8790;
+w=2,4080;
+teta=2,3880;
