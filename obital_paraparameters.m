11dab2e71d8549b1ce1a4adc46b58ebde4c3bcec
TeoBelo <64974940+TeoBelo@users.noreply.github.com>
2020-05-07 17:45:43 +0200

    Update obital_paraparameters.m

--- obital_paraparameters.m
+++ obital_paraparameters.m
@@ -2,63 +2,54 @@ function [vector_e,e,a,omega,i,w,teta,vector_h,vector_n] = obital_paraparameters
 % dati posizione e velocità iniziale calcolo parametri orbita
 % vector_e: vettore eccentricità
 % e: eccentricità
-% a: semiasse maggiore
-% omega: ascensione retta del nodo ascendente
-% i: inclinazione
-% w: anomalia pericentro
-% teta: anomalia reale
+% a: semiasse maggiore [km]
+% omega: ascensione retta del nodo ascendente [rad]
+% i: inclinazione [rad]
+% w: anomalia pericentro [rad]
+% teta: anomalia reale [rad]
 % vector_h: vettore qt. moto
 % vector_n: versore linea dei nodi
-% vector_r0: vettore posizione iniziale
-% vector_v0: vettore velocità iniziale
-% mu: cost. gravitazione specifica di un oggetto celeste
+% vector_r0: vettore posizione iniziale [km]
+% vector_v0: vettore velocità iniziale [km/s]
+% mu: cost. gravitazione specifica di un oggetto celeste [km^3/s^2]
 
 mu=398600;
-
 r0=norm(vector_r0);
-
 v0=norm(vector_v0);
 
+% calcolo vettore qtà moto
 vector_h=cross(vector_r0,vector_v0);
 
+% calcolo inclinazione
 i=acos(vector_h(3)/norm(vector_h));
 
+% calcolo eccentricità
 vector_e=cross(vector_v0,vector_h)/mu - vector_r0/r0;
-
 e=norm(vector_e);
 
+% calcolo semiasse maggiore
 a=0.5*mu*(mu/r0-(v0^2)*0.5)^(-1);
 
+% calcolo ascensione retta del nodo ascendente
 vector_n=cross([0 0 1],vector_h)/norm(cross([0 0 1],vector_h));
-
 if vector_n(2)>0
-    
     omega=acos(([1 0 0]*(vector_n)')/norm(vector_n));
-    
 else
-    
     omega=2*pi-acos(([1 0 0]*(vector_n)')/norm(vector_n));
 end
-omega=omega*180/pi;
 
+% calcolo anomalia pericentro
 if vector_e(3)>0
-    
     w=acos((vector_e*(vector_n)')/(e*norm(vector_n)));
-    
 else
-    
     w=2*pi-acos((vector_e*(vector_n)')/(e*norm(vector_n)));
 end
-w=w*180/pi;
-%prova
+
+% calcolo anomalia vera
 if vector_r0.*vector_v0>0
-    
     teta=acos(vector_e*(vector_r0)'/(e*r0));
-    
 else
-    
     teta=2*pi-acos(vector_e*(vector_r0)'/(e*r0));
 end
-teta=teta*180/pi;
 end
 
