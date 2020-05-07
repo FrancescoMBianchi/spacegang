079718836f441f61ecb011fcef1f61471849d5d5
albanbeshaj <64974585+albanbeshaj@users.noreply.github.com>
2020-05-07 18:54:14 +0200

    Update rv_parametri.m

--- rv_parametri.m
+++ rv_parametri.m
@@ -7,7 +7,8 @@ p=a*(1-e^2);
 rpf=[p*cos(theta)/(1+e*cos(theta));p*sin(theta)/(1+e*cos(theta));0];
 vpf=sqrt(mu/p)*[-sin(theta);e+cos(theta);0];
 
-%% passaggio a sistema equatoriale celeste
+%%passaggio a sistema equatoriale celeste
+
 %matrici di rotazione da I,J,K a pf 
 ROMEGA=[cos(OMEGA),sin(OMEGA),0; -sin(OMEGA),cos(OMEGA),0; 0,0,1];
 Ri=[1,0,0; 0,cos(i),sin(i); 0,-sin(i),cos(i)];
@@ -15,8 +16,8 @@ Romega=[cos(omega),sin(omega),0; -sin(omega),cos(omega),0; 0,0,1];
 Rtot=Romega*Ri*ROMEGA;
 
 %rotazione di rpf e vpf in I,J,K
-vect_r=Rtot'*rpf
-vect_v=Rtot'*vpf
+vect_r=Rtot'*rpf;
+vect_v=Rtot'*vpf;
 
 end
 
