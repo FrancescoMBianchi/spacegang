f83e075491f97740c3c76b43040555a51d63c31c
FrancescoMBianchi <63010568+FrancescoMBianchi@users.noreply.github.com>
2020-07-01 11:36:44 +0200

    Update changeOrbitalPlane.m
    più commenti e corretto errore segno

--- changeOrbitalPlane.m
+++ changeOrbitalPlane.m
@@ -5,6 +5,9 @@ mu=398600; %cost, andrebbe messa come var globale (!)
 p=ai*(1-ei^2);
 
 alpha=acos(cos(ii)*cos(i_f)+sin(ii)*sin(i_f)*cos(delta_omega)) %def angolo alpha
+
+%Troviamo  u1 e u2, calcolando i seni e coseni in modo da definirlo con il
+%segno corretto (infatti uso atan2)
 sinu1=sin(delta_omega)*sin(i_f)/sin(alpha); %Queste formule sono uguali per ogni possibile caso di delta_i e delta_omega (così come alpha)
 sinu2=sin(delta_omega)*sin(ii)/sin(alpha);
 
@@ -15,16 +18,16 @@ elseif delta_i<0
     cosu1=(cos(i_f)-cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
     cosu2=(-cos(ii)+cos(alpha)*cos(i_f))/(sin(alpha)*sin(i_f));
 elseif delta_i==0 &&delta_omega>0 %solo cambio ascensione retta del nodo ascendente, deltai=0
-    cosu2=(-cos(ii)+cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
-    cosu1=(cos(ii)-cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
-elseif delta_i==0 && delta_omega<0 %delta i=0 versione 2
-    cosu1=(-cos(ii)+cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
     cosu2=(cos(ii)-cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
+    cosu1=(-cos(ii)+cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
+elseif delta_i==0 && delta_omega<0 %delta i=0 versione 2
+    cosu1=(cos(ii)-cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
+    cosu2=(-cos(ii)+cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
 end
 
 u1=atan2(sinu1,cosu1)  %funz atan2 distingue il segno del seno e del coseno oltre a farne il rapporto
 u2=atan2(sinu2,cosu2)
-
+%trovati u1 e u2 dobbiamo torvare teta finale e wf
 if (delta_i*delta_omega)>0 || (delta_i==0 &&delta_omega>0) %se sono concordi o deltai=0 & deltaomega>0
     teta_f=u1-wi  %anomalia della manovra
     w_f=u2-teta_f  %arg periapsis nuovo
@@ -37,4 +40,5 @@ vteta=((mu/p)^0.5) *(1+ei*cos(teta_f)); %vel trasversale
 deltav1=2*vteta*sin(alpha/2) %calcolo deltav
 
 end
+
 
