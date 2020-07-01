447f41bd909817de937131e30eb23168e2ec788a
albanbeshaj <64974585+albanbeshaj@users.noreply.github.com>
2020-07-01 10:54:21 +0200

    Update changePeriapsisArg.m

--- changePeriapsisArg.m
+++ changePeriapsisArg.m
@@ -15,21 +15,13 @@ thetaorb2(2)=pi+deltaomega/2;
 thetaorb3(1)=2*pi-deltaomega/2;
 thetaorb3(2)=pi-deltaomega/2;
 
-%calcolo delle velocità in corrispondenza dei punti di intersezione (nei sistemi di riferimento perifocali)
-vorb2(1,:)=sqrt(mu/p)*[-sin(thetaorb2(1));ei+cos(thetaorb2(1))];
-vorb2(2,:)=sqrt(mu/p)*[-sin(thetaorb2(2));ei+cos(thetaorb2(2))];
-vorb3(1,:)=sqrt(mu/p)*[-sin(thetaorb3(1));ei+cos(thetaorb3(1))];
-vorb3(2,:)=sqrt(mu/p)*[-sin(thetaorb3(2));ei+cos(thetaorb3(2))];
-
-%calcolo dei deltav (due casi) 
-%riporto le velocità dell'orbita 2 nel sistema di riferimento perifocale 3
-R=[cos(deltaomega),sin(deltaomega);-sin(deltaomega),cos(deltaomega)];
-vorb2(1,:)=(R*(vorb2(1,:))')';       
-vorb2(2,:)=(R*(vorb2(2,:))')';
-deltav2(1)=norm(vorb2(1,:)-vorb3(1,:));
-deltav2(2)=norm(vorb2(2,:)-vorb3(2,:));
+%calcolo deltav
+deltav2=2*sqrt(mu/p)*ei*sin(deltaomega/2);
 
 %calcolo anomalia vera dell'orbita 3 (due casi)  
+%da aggiungere condizione del deltat per la scelta di uno o l'altro punto
+%di intersezione
+
 theta3(1)=thetaorb3(1);
 theta3(2)=thetaorb3(2);
 
