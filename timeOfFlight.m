8eed4077c9ac5d80911e5c22e2ccca935896eb9e
albanbeshaj <64974585+albanbeshaj@users.noreply.github.com>
2020-07-01 17:18:30 +0200

    Update timeOfFlight.m

--- timeOfFlight.m
+++ timeOfFlight.m
@@ -4,10 +4,19 @@ p=a*(1-e^(2));
 h=sqrt(mu*p);
 T=2*pi*sqrt(a^(3)/mu); %periodo orbita
 t=@(theta) ((1/((1-e^(2))^(3/2)))*(2*atan(sqrt((1-e)/(1+e))*tan(theta/2))-(e*sqrt(1-e^(2))*sin(theta))/(1+e*cos(theta))))*(h^(3)/(mu)^(2));
-if theta2>theta1
-    deltat12=t(theta2)-t(theta1);
-else
-    deltat12=t(theta2)-t(theta1)+T;
-    
+if theta1>pi
+    t_theta1=T/2+(T/2-t(2*pi-theta1));
+else t_theta1=t(theta1);
 end
 
+if theta2>pi
+    t_theta2=T/2+(T/2-t(2*pi-theta2));
+else t_theta2=t(theta2);
+end
+
+if theta2>theta1
+    deltat12=t_theta2-t_theta1;
+else
+    deltat12=t_theta2-t_theta1+T;
+    
+end
