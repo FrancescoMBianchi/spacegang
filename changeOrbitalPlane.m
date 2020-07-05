9da4a9b1e0c4eb4c21a257995044742c48f8dd1f
albanbeshaj <64974585+albanbeshaj@users.noreply.github.com>
2020-07-05 10:36:41 +0200

    Update changeOrbitalPlane.m

--- changeOrbitalPlane.m
+++ changeOrbitalPlane.m
@@ -4,7 +4,7 @@ delta_i=i_f-ii;
 mu=398600; %cost, andrebbe messa come var globale (!)
 p=ai*(1-ei^2);
 
-alpha=acos(cos(ii)*cos(i_f)+sin(ii)*sin(i_f)*cos(delta_omega)) %def angolo alpha
+alpha=acos(cos(ii)*cos(i_f)+sin(ii)*sin(i_f)*cos(delta_omega)); %def angolo alpha
 
 %Troviamo  u1 e u2, calcolando i seni e coseni in modo da definirlo con il
 %segno corretto (infatti uso atan2)
@@ -25,21 +25,21 @@ elseif delta_i==0 && delta_omega<0 %delta i=0 versione 2
     cosu2=(-cos(ii)+cos(alpha)*cos(ii))/(sin(alpha)*sin(ii));
 end
 
-u1=atan2(sinu1,cosu1)  %funz atan2 distingue il segno del seno e del coseno oltre a farne il rapporto
-u2=atan2(sinu2,cosu2)
+u1=atan2(sinu1,cosu1);  %funz atan2 distingue il segno del seno e del coseno oltre a farne il rapporto
+u2=atan2(sinu2,cosu2);
 %trovati u1 e u2 dobbiamo torvare teta finale e wf
 if (delta_i*delta_omega)>0 || (delta_i==0 &&delta_omega>0) %se sono concordi o deltai=0 & deltaomega>0
-    teta_f=u1-wi  %anomalia della manovra
+    teta_f=u1-wi; %anomalia della manovra
     %Ci sono due punti in cui fare la manovra, scelgo quello con delta v minore
     if cos(teta_f)>cos(teta_f+pi)
          teta_f=teta_f+pi;
          u1=u1+pi;
          u2=u2+pi;
-         disp('Scelto punto  di manovra più lontano')
+         %disp('Scelto punto  di manovra più lontano')
     end
-    w_f=u2-teta_f  %arg periapsis nuovo
+    w_f=u2-teta_f; %arg periapsis nuovo
 else %se sono discordi o deltai=0 & deltaomega<0
-    teta_f=2*pi-u1-wi
+    teta_f=2*pi-u1-wi;
     %Ci sono due punti in cui fare la manovra, scelgo quello con delta v minore
     if cos(teta_f)>cos(teta_f+pi)
          teta_f=teta_f+pi;
@@ -47,7 +47,7 @@ else %se sono discordi o deltai=0 & deltaomega<0
          u2=u2+pi;
          disp('Scelto punto  di manovra più lontano')
     end
-    w_f=2*pi-u2-teta_f
+    w_f=2*pi-u2-teta_f;
 end
 %check su
 %check sull'anomalia del perigeo
@@ -56,8 +56,7 @@ if w_f<0
 end
 
 vteta=((mu/p)^0.5) *(1+ei*cos(teta_f)); %vel trasversale
-deltav1=2*vteta*sin(alpha/2) %calcolo deltav
+deltav1=2*vteta*sin(alpha/2); %calcolo deltav
 
 end
-
 
